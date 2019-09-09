4ea9d01f6fbb5fabf944bc0b70255f7e3227edb5
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2019-09-09 12:04:47 +0200

    fix function names

--- init.py
+++ init.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
 import flask
-from   utils.cpwrap      import CFG
-import database.database as db
 
-app = flask.Flask(CFG("appName"))
+app = flask.Flask("serien-ampel")
 
 ##### FRONTEND PATHS ########
 @app.route('/')
 def rootPage():
     footer = flask.Markup(flask.render_template("partials/footer.html"))
     header = flask.Markup(flask.render_template("partials/header.html"))
-    header = flask.Markup(flask.render_template("partials/navbar.html"))
+    navbar = flask.Markup(flask.render_template("partials/navbar.html"))
     return flask.render_template("home.html", header=header, footer=footer)
 
-@app.route("/search-results")
-def tokenInputView():
+@app.route("/suggest-results")
+def suggestResults():
     '''This path displays results for a suggest with parameters'''
     footer = flask.Markup(flask.render_template("partials/footer.html"))
     header = flask.Markup(flask.render_template("partials/header.html"))
-    header = flask.Markup(flask.render_template("partials/navbar.html"))
-    return flask.render_template("search-results.html", header=header, footer=footer)
+    navbar = flask.Markup(flask.render_template("partials/navbar.html"))
+    return flask.render_template("suggest-results.html", header=header, footer=footer)
 
-@app.route("/suggest-results")
-def viewCreate():
+@app.route("/search-results")
+def searchResults():
     '''This path displays results for a series-search'''
     footer = flask.Markup(flask.render_template("partials/footer.html"))
     header = flask.Markup(flask.render_template("partials/header.html"))
-    header = flask.Markup(flask.render_template("partials/navbar.html"))
-    return flask.render_template("suggest-results.html", header=header, footer=footer)
+    navbar = flask.Markup(flask.render_template("partials/navbar.html"))
+    return flask.render_template("search-results.html", header=header, footer=footer)
 
 ##### STATIC FILES #####
 @app.route('/static/<path:path>')
@@ -39,5 +37,4 @@ def icon():
     return app.send_static_file('defaultFavicon.ico')
 
 if __name__ == "__main__":
-    db.init()
     app.run(host='0.0.0.0')
