fc082649d7de3b0cb229be262a1e46bff6447667
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2019-09-18 17:31:53 +0200

    implement basic login/session management

--- init.py
+++ init.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import flask
+import flask_login as fl
 import python.backend as backend
 
 app = flask.Flask("serien-ampel")
+app.secret_key             = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+loginManager               = fl.LoginManager()
 SEPERATOR = ","
 
 ##### FRONTEND PATHS ########
@@ -10,7 +14,9 @@ SEPERATOR = ","
 def rootPage():
     footer  = flask.Markup(flask.render_template("partials/footer.html"))
     header  = flask.Markup(flask.render_template("partials/header.html"))
-    navbar  = flask.Markup(flask.render_template("partials/navbar.html"))
+    navbar  = flask.Markup(flask.render_template("partials/navbar.html", user=fl.current_user))
+    
+    print(fl.current_user)
 
     options = backend.getFilters()
     #["Action", "SiFi", "Anime", "Crime"]
@@ -26,7 +32,7 @@ def suggestResults():
     '''This path displays results for a suggest with parameters'''
     footer  = flask.Markup(flask.render_template("partials/footer.html"))
     header  = flask.Markup(flask.render_template("partials/header.html"))
-    navbar  = flask.Markup(flask.render_template("partials/navbar.html"))
+    navbar  = flask.Markup(flask.render_template("partials/navbar.html"), user=fl.current_user)
     columNames = flask.Markup(flask.render_template("partials/seriesResultEntry.html", \
                                                                                 seriesTitle="Title", \
                                                                                 rank="#", \
@@ -48,7 +54,7 @@ def searchResults():
     '''This path displays results for a series-search'''
     footer = flask.Markup(flask.render_template("partials/footer.html"))
     header = flask.Markup(flask.render_template("partials/header.html"))
-    navbar = flask.Markup(flask.render_template("partials/navbar.html"))
+    navbar  = flask.Markup(flask.render_template("partials/navbar.html"), user=fl.current_user)
     columNames = flask.Markup(flask.render_template("partials/seriesResultEntry.html", \
                                                                                 seriesTitle="Title", \
                                                                                 rank="#", \
@@ -68,6 +74,43 @@ def staticFiles():
 def icon():
     return app.send_static_file('defaultFavicon.ico')
 
+
+##### USER SESSION MANAGEMENT #####
+class User(fl.UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_secret"
+        
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+@loginManager.user_loader
+def load_user(userId):
+    return User(hash(userId))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'POST':
+        username = flask.request.form['username']
+        password = flask.request.form['password']        
+        if password == username:
+            user = User(username)
+            fl.login_user(user)
+            return flask.redirect("/")
+        else:
+            return flask.abort(401)
+    else:
+        return flask.render_template('login.html')
+
+@app.route("/logout")
+@fl.login_required
+def logout():
+    fl.logout_user()
+    return flask.redirect("/")
+
 if __name__ == "__main__":
     backend.loadDB()
+    loginManager.init_app(app)
     app.run(host='0.0.0.0')
